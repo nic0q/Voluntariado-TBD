8d31ff65745aa13ad67a4490dd348a4b88b6657c
Nícolas Farfán <nicolas.farfan.ch@usach.cl>
2022-12-17 13:38:50 -0300

    [feat]: Project setup mongodb

---
+++ Frontend/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-dark text-dark">
+  <a class="navbar-brand text-light"  href="http://localhost:3000/home">Home</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link text-light" href="http://localhost:3000/habilidades-region">Habilidades por Región</a>
+      </li>
+    </ul>
+  </div>
+  </nav>
+</template>
